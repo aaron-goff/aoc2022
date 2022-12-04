5cc9779fdb82534a80dc8dd1d3540317e7cd7ef8
Aaron Goff <Aaron.Goff9@T-Mobile.com>
2022-12-04 13:13:09 -0500

    Day 4 Solution

---
+++ src/src/app/day4.kt
@@ -0,0 +1,28 @@
+package app
+
+fun main() {
+    val pairs = readFile("day4").map {
+        it.split(",").map { range ->
+            range.split("-")[0].toInt()..range.split("-")[1].toInt()
+        }
+    }
+    var contained = 0
+    var overlap = 0
+    pairs.forEach {
+        val first = it[0]
+        val second = it[1]
+        if ((first.contains(second.first) && first.contains(second.last)) ||
+            (second.contains(first.first) && second.contains(first.last))
+        ) {
+            contained++
+        }
+        if (first.contains(second.first) || first.contains(second.last) || second.contains(first.first) || second.contains(
+                first.last
+            )
+        ) {
+            overlap++
+        }
+    }
+    print("Number of completely contained pairs: $contained")
+    print("Number of overlapping pairs: $overlap")
+}
